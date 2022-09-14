e11e1639bd13874cfcd3d714c9a1bf5f5fffdcb8
Trevor Stephens <treversion@yahoo.com>
2022-09-14 10:37:02 -0700

    Update LibraryTest.java

--- inheritance/lib/src/test/java/inheritance/LibraryTest.java
+++ inheritance/lib/src/test/java/inheritance/LibraryTest.java
@@ -7,21 +7,20 @@ class LibraryTest {
     @Test
     void testingRestaurantRating() {
         Restaurant sut = new Restaurant("Cane's", 5);
-        assertEquals(5, 5);
+        assertEquals(5, sut.starRating);
     }
 
     @Test
     public void testAddingRestaurantName() {
         Restaurant sut = new Restaurant("Cane's", 5);
-        assertEquals("Cane's", "Cane's");
+        assertEquals("Cane's", sut.restaurantName);
     }
 
     @Test
     public void testAddReview() {
         Review sut = new Review("I am very unsure about testing", "Zork", 5);
-        assertEquals("I am very unsure about testing", "I am very unsure about testing");
+        assertEquals("I am very unsure about testing", sut.getBody());
         System.out.println(sut);
 
     }
 }
-
